9674c26b80c4e457b61f0cf364978c530b134421
Pavel Ignatev <palllkaignatev@gmail.com>
2022-11-13 00:44:43 +0300

    Коммит ради коммита

--- src/helpers/getWordParams.ts
+++ src/helpers/getWordParams.ts
@@ -20,36 +20,36 @@ const getWordParams = async (firstFile: Buffer, secondFile: Buffer) => {
   const variousInfo = firstFileParse.splice(di + 1, ai - di - 1);
 
   const [passportInfo, issuePassportDate] = variousInfo[0]
-    .replace(/,\s*$/, "")
-    .replace("выдан ", "")
-    .split(", ");
+    ?.replace(/,\s*$/, "")
+    ?.replace("выдан ", "")
+    ?.split(", ");
 
-  const passportInfoSplit = passportInfo.split(" ");
-  const passportType = passportInfoSplit.join(" ");
-  const [passportSeries, passportNumber] = passportInfoSplit.splice(
+  const passportInfoSplit = passportInfo?.split(" ");
+  const passportType = passportInfoSplit?.join(" ");
+  const [passportSeries, passportNumber] = passportInfoSplit?.splice(
     passportInfoSplit.length - 2,
     2
   );
-  const [surname, name, patronymic] = firstFileParse[sj + 1].split(" ");
+  const [surname, name, patronymic] = firstFileParse[sj + 1]?.split(" ");
   const [dateBirth, cityBirth] =
-    variousInfo[variousInfo.length - 1].split(", ");
+    variousInfo[variousInfo.length - 1]?.split(", ");
   const [passportIssuedBy, codePassportIssuedBy] = variousInfo
-    .splice(1, variousInfo.length - 2)
-    .join(" ")
-    .split(", к/п ");
+    ?.splice(1, variousInfo.length - 2)
+    ?.join(" ")
+    ?.split(", к/п ");
 
   return {
-    name,
-    surname,
-    patronymic,
-    passportType,
-    issuePassportDate,
-    dateBirth,
-    cityBirth,
-    passportSeries,
-    passportNumber,
-    passportIssuedBy,
-    codePassportIssuedBy,
+    name: name ?? "",
+    surname: surname ?? "",
+    patronymic: patronymic ?? "",
+    passportType: passportType ?? "",
+    issuePassportDate: issuePassportDate ?? "",
+    dateBirth: dateBirth ?? "",
+    cityBirth: cityBirth ?? "",
+    passportSeries: passportSeries ?? "",
+    passportNumber: passportNumber ?? "",
+    passportIssuedBy: passportIssuedBy ?? "",
+    codePassportIssuedBy: codePassportIssuedBy ?? "",
   };
 };
 
